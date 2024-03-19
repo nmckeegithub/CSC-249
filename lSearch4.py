02a07c910c1c3548a29406e5d5e8be0f7c4d6dcc
nmckeegithub <69421589+nmckeegithub@users.noreply.github.com>
2024-03-19 13:30:24 -0400

    Update lSearch4.py

--- lSearch4.py
+++ lSearch4.py
@@ -1,1 +1,7 @@
-
+lst=["kiran","arun","varun","kunnal","tiya","rhea" ]
+print("Checking if kunnal exists in list")
+exist_count = lst.count("kunnal")
+if exist_count> 0:
+   print("Yes, kunnal exists in list")
+else:
+   print("No, kunnal does not exists in list")
